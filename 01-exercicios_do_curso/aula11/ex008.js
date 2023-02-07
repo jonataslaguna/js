be09f2e217a22f2b77fca1aff439b3948a97521b
jonataslaguna <jonataslaguna.js@gmail.com>
2023-02-07 11:00:06 -0300

    finalizei aula 11

---
+++ 01-exercicios_do_curso/aula11/ex008.js
@@ -0,0 +1,9 @@
+var v = 80.5
+console.log(`A velocidade do seu carro é ${v}Km/h`) 
+if(v <= 80) {
+    console.log('Velocidade permitida.')
+    console.log('Digija sempre utilizando cinto de segurança.')
+} else{
+    console.log('Velocidade acima do permitido.')
+    console.log('Multado')
+}
