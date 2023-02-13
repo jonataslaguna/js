23e3191cbf25b2b4cc69f1496f3827a0d4822ed7
jonataslaguna <jonataslaguna.js@gmail.com>
2023-02-13 11:39:36 -0300

    Repetições

---
+++ 01-exercicios_do_curso/aula13/ambiente.js
@@ -0,0 +1,7 @@
+// WHILE (ENQUANTO)
+
+var c = 10
+while (c > 0){
+    console.log(c)
+    c-- // c <- c + 1
+}
---
+++ 01-exercicios_do_curso/aula13/ambiente02.js
@@ -0,0 +1,7 @@
+// Teste lógico no final
+
+var c = 1
+do {
+    console.log(c)
+    c++
+} while(c <= 10)
