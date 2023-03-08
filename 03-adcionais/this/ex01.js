f36061a331c3616e0d7e164846ae0a8187ee84ee
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-03-08 12:21:04 -0300

    primeiro ex com this

---
+++ 03-adcionais/this/ex01.js
@@ -0,0 +1,12 @@
+function aluno (nome,nota){
+    this.nome = nota
+    this.nota = nota
+
+    this.dadosAnonimos = function(){
+        setTimeout(function(){
+            
+        })
+    }
+}
+
+aluno('Jonatas', 10)
