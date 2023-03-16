55ec700d473f8bd57f295ccb0d4621b0ab84bc56
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-03-16 11:03:27 -0300

    Exercicio calculo imc

--- 03-adcionais/Dom/ex01/script.js
+++ 03-adcionais/Dom/ex01/script.js
@@ -1,4 +1,3 @@
-let form = document.querySelector('#form')
 let nome = document.getElementById('nome')
 let email = document.querySelector('#email')
 let pes = document.querySelector('#peso')
@@ -7,26 +6,33 @@ let res = document.querySelector('div#res')
 let res2 = document.querySelector('div#res2')
 
 function enviar(){
+    if(nome.value.length === 0 && pes.value.length === 0 && alt.value.length === 0){
+       alert('Por favor, preencha todos os campos obrigatórios')
+    }else{
+    const divParaMostrar = document.querySelector('div#res')
     let peso = Number(pes.value)
     let altura = Number(alt.value)
     let name = (nome.value)
     let imc = peso / altura ** 2
-    res.innerHTML = `Seu IMC é ${imc}`
+    
+    res.innerHTML = `Seu IMC é <strong>${imc.toFixed(2)}</strong>`
+    divParaMostrar.style.display = 'block';
+    
     if(imc < 17){
-        res2.innerHTML += `Olá ${name}, você está muito abaixo do peso.`
+        res2.innerHTML = `Olá ${name}, de acordo com o cálculo IMC, você está muito abaixo do peso.`
     } else if(imc >= 17 && imc <= 18.49){
-        res2.innerHTML += `Olá ${name}, você está abaixo do peso ideal`
+        res2.innerHTML = `Olá ${name}, de acordo com o cálculo IMC, você está abaixo do peso ideal`
     } else if (imc >= 18.5 && imc <= 24.99){
-        res2.innerHTML += `Olá ${name}, você está no peso ideal!`
+        res2.innerHTML = `Olá ${name}, de acordo com o cálculo IMC, você está no peso ideal!`
     } else if (imc >= 25 && imc <= 29.99){
-        res2.innerHTML += `Olá ${name}, você está um pouco acima do peso.`
+        res2.innerHTML = `Olá ${name}, de acordo com o cálculo IMC, você está um pouco acima do peso.`
     } else if (imc >= 30 && imc <= 34.99){
-        res2.innerHTML += `Olá ${name}, você está cima do peso`
+        res2.innerHTML = `Olá ${name}, de acordo com o cálculo IMC, você está cima do peso`
     } else if (imc >= 35 && imc >= 39.99){
-        res2.innerHTML += `Olá ${name}, você está em obesidade`
-    } else if (imc > 40){
-        res2.innerHTML += `Olá ${name}, você está em obesidade`
+        res2.innerHTML = `Olá ${name}, de acordo com o cálculo IMC, você está muito acima do peso`
+    } else if (imc > 40) {
+        res2.innerHTML = `Olá ${name}, de acordo com o cálculo IMC, você está em obesidade`
     }
-
+   }
 }
 
